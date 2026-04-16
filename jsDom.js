d22ece9df3d702e3559edf7bfeb3a3c1dc72e93d
prime Adal <igiranezaadal@gmail.com>
2026-04-16 19:19:55 +0200

    ijo

--- jsDom.js
+++ jsDom.js
@@ -89,20 +89,24 @@ function appendMessage(){
     msg.textContent="welcome to the world of DOM";
     document.body.appendChild(msg);
 }
+addPragraph();
+// appendMessage();
 
-function addParagraph() {
-    const para = document.createElement('p');
-    para.textContent = 'JavaScript is single threaded!';
 
-    appendNewMessage();
-    document.body.appendChild(para);
-}
 
-function appendNewMessage() {
-    const para = document.createElement('p');
+// function addParagraph() {
+//     const para = document.createElement('p');
+//     para.textContent = 'JavaScript is single threaded!';
 
-    para.textContent = "Isn't that cool?";
-    document.body.appendChild(para);
-}
+//     appendNewMessage();
+//     document.body.appendChild(para);
+// }
 
-addParagraph();
+// function appendNewMessage() {
+//     const para = document.createElement('p');
+
+//     para.textContent = "Isn't that cool?";
+//     document.body.appendChild(para);
+// }
+
+// addParagraph();
--- string.js
+++ string.js
@@ -1,22 +1,52 @@
-let greeting = "Hello, World!";
-let farewell = 'Goodbye!';
-let fullGreeting = greeting + ' ' + farewell;
-console.log(fullGreeting);
+// let greeting = "Hello, World!";
+// let farewell = 'Goodbye!';
+// let fullGreeting = greeting + ' ' + farewell;
+// console.log(fullGreeting);
 
-let name= "Adal igiraneza ";
-let age=18;
-let place="kigali";
-console.log("\nhis name is " +name+", he is "+ age +" years old, and he lives in "+place)
+// let name= "Adal igiraneza ";
+// let age=18;
+// let place="kigali";
+// console.log("\nhis name is " +name+", he is "+ age +" years old, and he lives in "+place)
 
-console.log("\nstring leng of place= "+place.length)
-console.log("string leng of name= "+name.length)
+// console.log("\nstring leng of place= "+place.length)
+// console.log("string leng of name= "+name.length)
 
-console.log("\naccessing characters of the strinng: "+ name[1])
-console.log("accessing characters of the strinng: "+ place[0])
+// console.log("\naccessing characters of the strinng: "+ name[1])
+// console.log("accessing characters of the strinng: "+ place[0])
 
-let text = "HELLO WORLD";
-let char = text.charAt(0);
-console.log("\n "+char);
-for (var i = 0; i < 10; i++) {
-  console.log(i)
+// let text = "HELLO WORLD";
+// let char = text.charAt(0);
+// console.log("\n "+char);
+// for (var i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+
+
+
+// function rowSumOddNumbers(n) {
+// function rowSumOddNumbersTrianglePattern(n) {
+//   let sum = 0;
+//   let oddNumber = 1; // Start with the first odd number (1)
+//   for (let i = 0; i < n; i++) {
+//     sum += oddNumber; // Add the current odd number to the sum
+//     oddNumber += 2; // Move to the next odd number
+//   }
+//   return sum;
+// }
+function pramidPattern(n) {
+  let c=1;
+  for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= n - i; j++) {
+      row += '  '; // Add spaces for alignment
+    }
+    for (let j = 1; j <= i; j++) {
+      
+      row += '  '+c+' ';
+      c+=2;
+    }
+    console.log(row);
+  }
 }
+
+pramidPattern(9);
