1c7bcec73d90e9f8273fcb764942f33ed27af778
prime Adal <igiranezaadal@gmail.com>
2026-06-17 12:06:07 +0200

    array course

---
+++ fundamentals/array.js
@@ -0,0 +1,12 @@
+let array=["Banana", "Orange", "Apple", "Mango"]
+console.log("Before sort");
+console.log(array)
+console.log("After sort");
+console.log(array.sort())
+//min
+let number=[6,57,5]
+console.log(number)
+Math.min.apply(null,number)
+console.log(number);
+
+
