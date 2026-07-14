73b8ff361a67d18a9c00f75651a217e1967142db
Adal <igiranezaadal@gmail.com>
2026-07-14 14:01:53 +0200

    delete most times

---
+++ Codewars/deleteMostTimes.js
@@ -0,0 +1,28 @@
+function sc(arr){
+  
+  let c=0;let repeat;let newArr;
+  for(let a=0;a<=arr.length;a++){
+    if(arr[a]==undefined && arr[1]==undefined) return arr
+    // if(repeat=a[0])continue
+  }
+  
+}
+console.log(sc([[3,3,3,3,3,3,3,3,3]]))
+console.log(sc([[3,4,5]]))
+console.log(sc([[3,4,4,5],[3,4,4,5],[3,4,4,5],[3,4,4,5]]))
+console.log(sc([]))
+console.log(sc([[]]))
+
+// const myGirls=[];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+// const [name1st, ...theRest]=myBoys
+// console.log(name1st)
+
+
+// const myChildren = myGirls.concat(myBoys);
+// console.log(myChildren.join(''))
+// console.log(myGirls)
+// if(myGirls[0]==undefined){
+//   console.log('gg undefined')
+// }
+// else console.log(' errors')
