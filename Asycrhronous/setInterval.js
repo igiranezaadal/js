c43dc2f95cde42139799a60b57f023cd9d37a9ac
Adal <igiranezaadal@gmail.com>
2026-08-19 12:20:28 +0200

    set Interval

--- Asycrhronous/setInterval.js
+++ Asycrhronous/setInterval.js
@@ -1,13 +1,26 @@
-let count = 0;
+// let count = 0;
 
-// Execute every 1000 milliseconds (1 second)
-const iterate = setInterval(() => {
-  count++;
-  console.log(`Interval count: ${count}`);
+// // Execute every 1000 milliseconds (1 second)
+// const counting = setInterval(() => {
+//   count++;
+//   console.log(`Interval count: ${count}`);
   
-  // Stop the interval after 3 iterations
-  if (count === 3) {
-    // clearInterval(iterate);
-    console.log("Interval cleared");
+//   // Stop the interval after 5 iterations
+//   if (count === 5) {
+//     // clearInterval(iterate);
+//     clearInterval(counting)
+//     console.log("Interval cleared");
+//   }
+// }, 1000);
+
+//setInterval is a built-in JavaScript function that repeatedly executes a specified function or code snippet at fixed time intervals (in milliseconds). It continues to run until it is explicitly stopped using clearInterval.
+let counter=0
+console.log('start');
+
+let ada=setInterval(()=>{
+  console.log(`counter ${counter++}`);
+  if (counter==4) {
+    clearInterval(ada)
+    console.log('end interval');
   }
-}, 1000);
+},1000)
