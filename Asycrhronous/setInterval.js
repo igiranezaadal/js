527a367d327af6503c5087e1d0275fc96aca9f3a
Adal <igiranezaadal@gmail.com>
2026-08-28 11:23:26 +0200

    ordering fetch

--- Asycrhronous/setInterval.js
+++ Asycrhronous/setInterval.js
@@ -14,13 +14,44 @@
 // }, 1000);
 
 //setInterval is a built-in JavaScript function that repeatedly executes a specified function or code snippet at fixed time intervals (in milliseconds). It continues to run until it is explicitly stopped using clearInterval.
-let counter=0
-console.log('start');
+// let counter=0
+// console.log('start');
 
-let ada=setInterval(()=>{
-  console.log(`counter ${counter++}`);
-  if (counter==4) {
-    clearInterval(ada)
-    console.log('end interval');
-  }
+// let ada=setInterval(()=>{
+//   console.log(`counter ${counter++}`);
+//   if (counter==4) {
+//     clearInterval(ada)
+//     console.log('end interval');
+//   }
+// },1000)
+
+
+// Write a JavaScript program to implement a function that executes a given function repeatedly at a fixed interval using 'setInterval()'.
+let counter=1
+let times=setInterval(()=>{
+    console.log(`hello ${counter++} times`);
+    if(counter==6) {
+        clearInterval(times)   
+        console.log('Execution stopped.');
+ 
+    }
 },1000)
+
+
+
+// function repeat_Function(fn, interval) {
+//   fn();
+//   const intervalId = setInterval(fn, interval);
+//   return function stopExecution() {
+//     clearInterval(intervalId);
+//     console.log('Execution stopped.');
+//   };
+// }
+// const intervalMs = 1000;
+// function printMessage() {
+//   console.log('Executing the function...');
+// }
+// const stopExecution = repeat_Function(printMessage, intervalMs);
+// setTimeout(() => {
+//   stopExecution();
+// }, 4000);
