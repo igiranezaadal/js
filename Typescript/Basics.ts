b815e0d587157e75a6f031ed066dbff956ac04a4
Adal <igiranezaadal@gmail.com>
2026-09-15 15:55:02 +0200

    types and type annotation in TypeScript

--- Typescript/Basics.ts
+++ Typescript/Basics.ts
@@ -1,6 +1,6 @@
 // let txt="typescript is superset of Js"
 // console.log(txt);
-
+// array
 // let ids: number[]=[1,2,3,4,5]
 // let arr: any[]=[`heloo ${ids}`, 2 , 0.001 , ids , true]
  
@@ -27,9 +27,33 @@ let obj={
 // console.log(obj[unique_key])
 // console.log('testing');
 
-// null
+// null undefined
 let empty: null=null
+let ud: undefined
 console.log(empty)
+console.log(ud)
+// object
+let adal:object ={ name: 'bob' }
+// console.log(adal);
+// function void
+function log():void{
+    console.log('log');
+    // return 'return'
+}
+log()
 
+// Enum(enumeration ): is collection of reelated values that can be numeric or string
+// enum  color {red, green, blue}
+// let c: color=color.green
+// console.log(c);
+ 
+// any : is supertype of all datatypes in TypeScript
+let someThing: any =adal 
+console.log(someThing);
 
+// type vs interface
+type User={ name: string}
+let user: User ={name: "type in TS"}
+console.log(user);
+// type annotation: is basically giving type to the variable in TypeScript
 
