f5139ee6f8c3fb390c98b345dfd90420f3334c28
Adal <igiranezaadal@gmail.com>
2026-09-15 14:09:57 +0200

    tuples in typescript

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -46,3 +46,82 @@
 // }
 // console.log(stringToNumber("43"));
 
+/**
+ * Challenge: Fix the TS warnings about orderQueue!
+ */
+
+// type Pizza = {
+//     name: string
+//     price: number
+// }
+
+// type Order = {
+//     id: number
+//     pizza: Pizza
+//     status: string
+// }
+
+// const menu = [
+//     { name: "Margherita", price: 8 },
+//     { name: "Pepperoni", price: 10 },
+//     { name: "Hawaiian", price: 10 },
+//     { name: "Veggie", price: 9 },
+// ]
+
+// let cashInRegister = 100
+// let nextOrderId = 1
+// const orderQueue:Order[] = []
+
+// function addNewPizza(pizzaObj: Pizza) {
+//     menu.push(pizzaObj)
+// }
+
+// function placeOrder(pizzaName: string) {
+//     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+//     if (!selectedPizza) {
+//         console.error(`${pizzaName} does not exist in the menu`)
+//         return
+//     }
+//     cashInRegister += selectedPizza.price
+//     const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+//     orderQueue.push(newOrder)
+//     return newOrder
+// }
+
+// function completeOrder(orderId: number) {
+//     const order = orderQueue.find(order => order.id === orderId)
+//     if(!order){
+//         console.error(`${order} does not exist in the menu`)
+//         return
+//     }
+//     order.status = "completed"
+//     return order
+// }
+
+// addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+// addNewPizza({ name: "BBQ Chicken", price: 12 })
+// addNewPizza({ name: "Spicy Sausage", price: 11 })
+
+// placeOrder("Chicken Bacon Ranch")
+// completeOrder(1)
+
+// console.log("Menu:", menu)
+// console.log("Cash in register:", cashInRegister)
+// console.log("Order queue:", orderQueue)
+
+
+
+function f(n:number, m:number) {
+  // My function body is ready
+  let sum=0;
+  for (let i=1;i<=n;i++){
+    if(i%m){
+      sum+=m
+    }
+    else continue
+  }
+  return sum/2
+}
+console.log(f(10, 5)) // 20
+console.log(f(20, 20)) // 190
+console.log(f(15, 10)) // 60
--- Typescript/src/str.ts
+++ Typescript/src/str.ts
@@ -1,6 +1,2 @@
 let txt="typescript is my momy"
-if(!txt || txt==undefined){
-    throw new Error('texting eroor')
-}
-else
 console.log(txt);
