5b0ea308401fb64a40b660db1cafe186fb9e3a6e
Adal <igiranezaadal@gmail.com>
2026-09-15 14:48:51 +0200

    symbols in typeScript

---
+++ Typescript/Basics.ts
@@ -0,0 +1,35 @@
+// let txt="typescript is superset of Js"
+// console.log(txt);
+
+// let ids: number[]=[1,2,3,4,5]
+// let arr: any[]=[`heloo ${ids}`, 2 , 0.001 , ids , true]
+ 
+// // tuples
+// let person: [number, String,boolean]=[67, 'hey', false]
+// console.log(ids);
+// console.log(arr.sort((a,b)=>b-a));
+// console.log(person);
+
+// // tuples arr
+// let employee: [number, string][]=[
+//     [1, 'adal'],
+//     [2, 'others1'],
+//     [3, 'others2']
+// ]
+// console.log(employee);
+
+// Symbol
+const unique_key=Symbol();
+let obj={
+    [unique_key]: "secretValue"
+}
+// console.log(obj)
+// console.log(obj[unique_key])
+// console.log('testing');
+
+// null
+let empty: null=null
+console.log(empty)
+
+
+
