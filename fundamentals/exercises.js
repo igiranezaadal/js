794dcdf991e3e357ef1c59c0df5f40bbff4e82cc
Adal <igiranezaadal@gmail.com>
2026-09-22 09:07:04 +0200

    partern diamond or triangle

---
+++ fundamentals/exercises.js
@@ -0,0 +1,60 @@
+// function transformToObject(users){
+//     const result = {};
+//     for (const user of users) {
+//         const id = user.id;
+//         const name = user.name.split(' ')[0];// Get the last name if it exists, otherwise get the first name
+//         result[id] = name;
+//     }
+//     return result;
+// }
+// const users = [
+//   { id: 1, name: 'Alicen' },
+//   { id: 2, name: 'Bob marley' },
+//   { id: 3, name: 'Charlie kirk' },
+// ];
+
+// const transformed = transformToObject(users);
+// console.log(transformed); // Output: { '1': 'Alice', '2': 'Bob', '3': 'Charlie' }
+
+
+
+
+
+// let myObject = { oldKey: "some data" };
+// myObject["newKey"] = myObject["oldKey"];
+// delete myObject["oldKey"];
+
+
+
+// function lexicalScope(){
+//   let count=0
+//   return function(){
+//     count+=1
+//     return count
+//   }
+// }
+// console.log((lexicalScope));
+// let counter=lexicalScope();
+// const max=6
+// for(let c=1;c<max;c++){
+// console.log(counter());
+// }
+
+
+
+// let a =Symbol(1);
+// console.log(typeof(a));
+
+
+// partern
+let n=10,pat="";
+for(let i=1;i<=n;i++){
+    for(let j=1;j<=n-i;j++){
+        pat+=" "
+    }
+    for(let j=1;j<=i;j++){
+        pat+="* "
+    }
+    pat+='\n';
+}
+console.log(pat);
